7839e2a9c684f89de85d8e8e48285165ead91d4a
Lucas Farias <lucas_fnf@hotmail.com>
2020-01-05 12:00:11 -0300

    Add Logger draft

---
+++ include/engine/logger/logger.hpp
@@ -0,0 +1,50 @@
+/**
+ * Engine
+ * © Lucas "Hukasu" Farias
+ */
+
+#ifndef __LOGGER__HPP__
+#define __LOGGER__HPP__
+
+#include <string>
+#include <ostream>
+#include <queue>
+#include <atomic>
+#include <thread>
+#include <condition_variable>
+
+#define logTrace(x1, x2) x1.log(LoggerLevel::eTrace, x2, std::string(__FILE__), __LINE__)
+#define logInfo(x1, x2) x1.log(LoggerLevel::eInformation, x2, std::string(__FILE__), __LINE__)
+#define logWarning(x1, x2) x1.log(LoggerLevel::eWarning, x2, std::string(__FILE__), __LINE__)
+#define logError(x1, x2) x1.log(LoggerLevel::eError, x2, std::string(__FILE__), __LINE__)
+
+namespace engine {
+    enum class LoggerLevel {
+        eTrace,
+        eInformation,
+        eWarning,
+        eError,
+        eVerbose = eTrace
+    };
+    typedef LoggerLevel MessageSeverity;
+
+    class Logger {
+        std::atomic<bool> m_thread_running;
+        std::thread m_logger_thread;
+        std::ostream& m_output;
+        LoggerLevel m_logger_level;
+        std::mutex m_message_queue_mutex;
+        std::condition_variable m_message_queue_cv;
+        std::queue<std::string> m_message_queue;
+
+        void loggerThread();
+    public:
+        Logger(std::ostream& _output, LoggerLevel _logger_level);
+        ~Logger();
+
+        void changeLoggerLevel(LoggerLevel _logger_level);
+        void log(MessageSeverity _message_severity, std::string _message, std::string _file, uint32_t _line);
+    };
+}
+
+#endif // __LOGGER__HPP__
---
+++ src/engine/logger/logger.cpp
@@ -0,0 +1,64 @@
+#include <engine/logger/logger.hpp>
+
+#include <sstream>
+
+namespace engine {
+	std::ostream& operator<<(std::ostream& _os, const LoggerLevel _logger_level) {
+		switch (_logger_level) {
+		case LoggerLevel::eTrace:
+			_os << "[TRACE]";
+			break;
+		case LoggerLevel::eInformation:
+			_os << "[INFO ]";
+			break;
+		case LoggerLevel::eWarning:
+			_os << "[WARN ]";
+			break;
+		case LoggerLevel::eError:
+			_os << "[ERROR]";
+			break;
+		default:
+			_os << "[?????]";
+			break;
+		}
+		return _os;
+	}
+
+	Logger::Logger(std::ostream& _output, LoggerLevel _logger_level) : m_output(_output), m_logger_level(_logger_level) {
+		m_thread_running = true;
+		m_logger_thread = std::thread(&Logger::loggerThread, this);
+	}
+
+	Logger::~Logger() {
+		m_thread_running = false;
+		m_message_queue_cv.notify_one();
+		m_logger_thread.join();
+	}
+
+	void Logger::loggerThread() {
+		while (m_thread_running) {
+			std::unique_lock<std::mutex> lck(m_message_queue_mutex);
+			m_message_queue_cv.wait(lck);
+			while (!m_message_queue.empty()) {
+				std::string message = m_message_queue.front();
+				m_message_queue.pop();
+				m_output << message;
+			}
+		}
+	}
+
+	void Logger::changeLoggerLevel(LoggerLevel _logger_level) {
+		m_logger_level = _logger_level;
+	}
+
+	void Logger::log(MessageSeverity _message_severity, std::string _message, std::string _file, uint32_t _line) {
+		if (!m_thread_running || _message_severity < m_logger_level) return;
+		std::stringstream message_builder;
+		message_builder << _message_severity << " " << _file << ":" << _line << ": " << _message;
+		std::string message = message_builder.str();
+		std::unique_lock lck(m_message_queue_mutex);
+		m_message_queue.push(message);
+		m_message_queue_cv.notify_one();
+	}
+}
+
