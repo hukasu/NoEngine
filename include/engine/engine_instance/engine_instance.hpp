3b9f828a4a80d5abc5947a9875b7de12821c3fa6
Lucas Farias <lucas_fnf@hotmail.com>
2020-01-05 12:31:25 -0300

    Add Logger to EngineInstanceCreateInfo Write documentation

--- include/engine/engine_instance/engine_instance.hpp
+++ include/engine/engine_instance/engine_instance.hpp
@@ -6,41 +6,67 @@
 #ifndef __ENGINE__INSTANCE__HPP__
 #define __ENGINE__INSTANCE__HPP__
 
+#include "../logger/logger.hpp"
 #include <string>
 
 namespace engine {
 	class EngineInstance_T;
 
-	// Structure used in the creation of EngineInstances
+	/// <summary>Structure used in the creation of EngineInstances</summary>
 	struct EngineInstanceCreateInfo {
-		// Application name
+		/// <summary>Application;s name</summary>
 		std::string m_application_name;
-		// Application Version
+		/// <summary>Application's Version</summary>
 		uint32_t m_application_version;
+		/// <summary>Engine's Logger</summary>
+		Logger* m_logger;
 
+		/// <summary>Creates and EngineInstanceCreateInfo object</summary>
+		/// <param name='_application_name>Application's name</param>
+		/// <param name='_application_name>Application's version</param>
+		/// <param name='_logger>Engine's logger, setting this parameter implicitly enables logging of engine operations</param>
 		EngineInstanceCreateInfo(
 			std::string _application_name = "",
-			uint32_t _application_version = 0
+			uint32_t _application_version = 0,
+			Logger* _logger = nullptr
 		);
 
-		// Chain setter for Application Name
+		/// <summary>Chain setter for Application Name</summary>
+		/// <param name='_application_name>Application's name</param>
 		EngineInstanceCreateInfo& setApplicationName(std::string _application_name);
-		// Chain setter for Application Version
+		/// <summary>Chain setter for Application Version</summary>
+		/// <param name='_application_name>Application's version</param>
 		EngineInstanceCreateInfo& setApplicationVersion(uint32_t _application_version);
+		/// <summary>Chain setter for Logger</summary>
+		/// <param name='_application_name>Engine's logger, setting this parameter implicitly enables logging of engine operations</param>
+		EngineInstanceCreateInfo& setLogger(Logger* _logger);
 	};
 
+	/// <summary>Engine's starting point</summary>
 	class EngineInstance {
+		/// <summary>Pointer to internal objects</summary>
 		EngineInstance_T* m_engine_instance_t;
 	public:
+		/// <summary>Empty constructor</summary>
 		EngineInstance();
+		/// <summary>Null constructor</summary>
 		EngineInstance(std::nullptr_t);
+		/// <summary>Complete constructor</summary>
 		EngineInstance(EngineInstance_T* _engine_instance);
 
+		/// <summary>Verifies if EngineInstances are equal</summary>
+		/// <param name='_rhs'>The other EngineInstance</param>
 		bool operator==(EngineInstance const& _rhs) const;
+		/// <summary>Verifies if current EngineInstance is smaller than <paramref name='_rhs' /></summary>
+		/// <param name='_rhs'>The other EngineInstance</param>
 		bool operator<(EngineInstance const& _rhs) const;
+		/// <summary>Verifies if current EngineInstance is valid</summary>
 		operator bool() const;
+		/// <summary>Cast to internal object</summary>
 		operator EngineInstance_T* () const;
 
+		/// <summary>Destroys EngineInstance</summary>
+		/// <remarks>Any undestroyed child object will be destroyed</remarks>
 		void destroy();
 	};
 
--- src/engine/engine_instance/engine_instance_create_info.cpp
+++ src/engine/engine_instance/engine_instance_create_info.cpp
@@ -3,21 +3,26 @@
 namespace engine {
 	EngineInstanceCreateInfo::EngineInstanceCreateInfo(
 		std::string _application_name,
-		uint32_t _application_version
+		uint32_t _application_version,
+		Logger* _logger
 	) {
 		m_application_name = _application_name;
 		m_application_version = _application_version;
+		m_logger = _logger;
 	}
 
-	// Chain setter for Application Name
 	EngineInstanceCreateInfo& EngineInstanceCreateInfo::setApplicationName(std::string _application_name) {
 		m_application_name = _application_name;
 		return *this;
 	}
 
-	// Chain setter for Application Version
 	EngineInstanceCreateInfo& EngineInstanceCreateInfo::setApplicationVersion(uint32_t _application_version) {
 		m_application_version = _application_version;
+		return *this;
+	}
+
+	EngineInstanceCreateInfo& EngineInstanceCreateInfo::setLogger(Logger* _logger) {
+		m_logger = _logger;
 		return *this;
 	}
 }
