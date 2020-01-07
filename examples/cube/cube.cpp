a69efe16729e13fd4f4c6d55a765769d7b3685d0
Lucas Farias <lucas_fnf@hotmail.com>
2020-01-07 16:43:04 -0300

    Add logger to cube example

--- examples/cube/cube.cpp
+++ examples/cube/cube.cpp
@@ -1,9 +1,12 @@
 #include <engine/engine.hpp>
+#include <iostream>
 
 int main(int argc, char** argv) {
+	engine::Logger logger(std::cout, engine::LoggerLevel::eTrace);
 	engine::EngineInstanceCreateInfo engine_instance_info = {
 		"Cube",
-		engine::makeVersionNumber(0, 0, 0)
+		engine::makeVersionNumber(0, 0, 0),
+		&logger
 	};
 	engine::EngineInstance engine_instance = engine::createEngineInstance(&engine_instance_info);
 	engine_instance.destroy();
